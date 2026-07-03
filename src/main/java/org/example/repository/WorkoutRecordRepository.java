d70fe70b75d26c14088d0db69320d270cc23dfc9
22200324 <parkwlgh3484@handong.ac.kr>
2026-07-03 23:00:11 +0900

    repository 인터페이스 구현

--- src/main/java/org/example/repository/WorkoutRecordRepository.java
+++ src/main/java/org/example/repository/WorkoutRecordRepository.java
@@ -1,4 +1,19 @@
 package org.example.repository;
 
+import org.example.model.WorkoutRecord;
+
+import java.util.List;
+import java.util.Optional;
+
 public interface WorkoutRecordRepository {
+
+    WorkoutRecord save(WorkoutRecord record);
+
+    List<WorkoutRecord> findAll();
+
+    Optional<WorkoutRecord> findById(Long id);
+
+    boolean update(WorkoutRecord record);
+
+    boolean deleteById(Long id);
 }
